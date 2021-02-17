50cd8ef9d5f1df1f14cc2f348cb6e89c40d24580
Thomas Cazade <cazade.thomas@gmail.com>
2021-02-17 17:19:42 +0100

    Clean boilerplate

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,15 +4,18 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'hgmfrance',
+    title: 'H.G.M. France',
+
     htmlAttrs: {
-      lang: 'en',
+      lang: 'fr',
     },
+
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
     ],
+
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
 
