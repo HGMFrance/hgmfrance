591cd12558cd4c0f1489bbf65cd31687731ae3bc
Thomas Cazade <cazade.thomas@gmail.com>
2021-02-17 20:45:07 +0100

    Add Partners component

---
+++ components/Partners.vue
@@ -0,0 +1,28 @@
+<template>
+  <section
+    class="flex items-center justify-center py-16 border-t border-gray-300 bg-gray-50"
+  >
+    <div
+      class="flex flex-row items-center justify-center lg:max-w-5xl xl:max-w-7xl overflow-x-hidden"
+    >
+      <img
+        v-for="(image, i) in images"
+        :key="`image-${i}`"
+        class="px-3 py-3 mr-6 inline-block w-40 h-auto rounded-md border bg-white border-gray-100"
+        :src="image"
+        alt=""
+      />
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    images: {
+      type: Array,
+      required: true,
+    },
+  },
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,16 @@
 <template>
   <div>
     <Carousel :slides="slides" />
-
-    <div class="lg:max-w-5xl xl:max-w-7xl mx-auto px-16"></div>
+    <Partners :images="partners" />
   </div>
 </template>
 
 <script>
 import Carousel from '@/components/Carousel'
+import Partners from '@/components/Partners'
 
 export default {
-  components: { Carousel },
+  components: { Carousel, Partners },
 
   data() {
     return {
@@ -40,6 +40,15 @@ export default {
             'Aliquam arcu eros, mattis vitae tellus sit amet, commodo dignissim neque.',
           learnMoreLink: 'https://google.com/',
         },
+      ],
+
+      partners: [
+        'http://demo.themenio.com/industrial/image/cl-logo1.png',
+        'http://demo.themenio.com/industrial/image/cl-logo2.png',
+        'http://demo.themenio.com/industrial/image/cl-logo3.png',
+        'http://demo.themenio.com/industrial/image/cl-logo4.png',
+        'http://demo.themenio.com/industrial/image/cl-logo5.png',
+        'http://demo.themenio.com/industrial/image/cl-logo6.png',
       ],
     }
   },
