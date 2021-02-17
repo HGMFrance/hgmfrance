5cb139bbaf5d183c91441614980e8505624cadbc
Thomas Cazade <cazade.thomas@gmail.com>
2021-02-17 21:36:06 +0100

    Add meta-descriptions

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,7 +13,39 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      {
+        hid: 'description',
+        name: 'description',
+        content:
+          '[DÉVELOPPEMENT] Conception, fabrication et réalisation de systèmes pour lignes de conditionnement incluant le convoyage, le contrôle vision ou des machines spéciales.',
+      },
+      {
+        hid: 'og:url',
+        property: 'og:url',
+        content: 'https://hgmfrance.totominc.io/',
+      },
+      {
+        hid: 'og:type',
+        property: 'og:type',
+        content: 'article',
+      },
+      {
+        hid: 'og:title',
+        property: 'og:title',
+        content:
+          'H.G.M. France — Conception de systèmes pour lignes de conditionnement',
+      },
+      {
+        hid: 'og:description',
+        property: 'og:description',
+        content:
+          '[DÉVELOPPEMENT] Conception, fabrication et réalisation de systèmes pour lignes de conditionnement incluant le convoyage, le contrôle vision ou des machines spéciales.',
+      },
+      {
+        hid: 'og:image',
+        property: 'og:image',
+        content: 'http://demo.themenio.com/industrial/image/post-thumb-c.jpg',
+      },
     ],
 
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
