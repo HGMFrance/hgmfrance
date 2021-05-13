4bbab021c789d29bf0a26974ebad60b62077ad14
Thomas Cazade <cazade.thomas@gmail.com>
2021-05-13 18:42:41 +0200

    Update AlertBanner background-color

--- components/AlertBanner.vue
+++ components/AlertBanner.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="relative w-full bg-orange-300">
+  <div class="relative w-full bg-orange-200">
     <div class="mx-auto max-w-7xl px-4 lg:px-16 py-2 lg:py-3 text-center font-roboto">
       <p class="text-sm lg:text-lg font-source font-bold" v-html="text"></p>
     </div>
