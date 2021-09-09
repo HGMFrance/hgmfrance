1ae5b720f8d90d6fa5be2a78761a71f968113212
Thomas Cazade <cazade.thomas@gmail.com>
2021-09-09 09:38:19 +0200

    Make AlertBanner redirect to /actualites on click

--- components/AlertBanner.vue
+++ components/AlertBanner.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="relative w-full bg-orange-200">
+  <nuxt-link to="/actualites" class="relative block w-full bg-orange-200 cursor-pointer">
     <div class="mx-auto max-w-7xl px-4 lg:px-16 py-2 lg:py-3 text-center font-roboto">
       <p class="text-sm lg:text-lg font-source font-bold" v-html="text"></p>
     </div>
-  </div>
+  </nuxt-link>
 </template>
 
 <script>
